19bcf368aeb10fa3add3ec70d9fd159573ddc9e9
Elias Athey <athey.elias@gmail.com>
2017-04-22 21:14:11 -0600

    Changed home file to work with media plugin.

--- frontend/Chordian/src/pages/home/home.ts
+++ frontend/Chordian/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { MediaPlugin, MediaObject } from '@ionic-native/media';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	mediaPlug: MediaPlugin;
+	mediaObj: MediaObject;
 
-  }
+	constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+	}
+
+	ionViewDidEnter(){
+		this.mediaPlug = new MediaPlugin();
+		this.mediaPlug.create('/android_asset/www/assets/audio/audio_input.mp3').then(mediaObj => this.mediaObj = mediaObj);
+	}
+
+	startRecording(){
+		this.mediaObj.startRecord();
+	}
+
+	stopRecording(){
+		this.mediaObj.stopRecord();
+	}
+
+	playRecording(){
+		this.mediaObj.play();
+	}
+
+	showAlert(message){
+		let alert = this.alertCtrl.create({
+			title: 'Error',
+			subTitle: message,
+			buttons: ['OK']
+		});
+		alert.present();
+	}
+
+
 
 }
